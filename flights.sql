1af4674a1095dc83989e65fbf3c3fabc383e9795
Deji Adegbite <d2alphame@yahoo.com>
2021-02-09 22:22:23 +0100

    (WIP): flights.sql

--- flights.sql
+++ flights.sql
@@ -9,3 +9,15 @@ create table flights (
 	destination_city varchar(128) not null,
 	flight_duration tinyint not null
 );
+
+create table bookings (
+	booking_id integer primary key,
+	booking_number integer not null,
+	flight_number integer not null,
+	seat_number char(4) not null,
+	first_name varchar(32) not null,
+	last_name varchar(32) not null,
+	middle_name varchar(32) not null,
+	email varchar(128) not null,
+	phone char(32)
+)
