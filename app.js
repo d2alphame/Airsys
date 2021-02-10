6aae87576341feae0fb256b1c3261d332ee674b1
Deji Adegbite <d2alphame@yahoo.com>
2021-02-10 16:48:55 +0100

    Restart and reinstall

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var flightsRouter = require('./routes/flights');
-var bookingsRouter = require('./routes/bookings');
 
 var app = express();
 
@@ -23,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/flights', flightsRouter);
-app.use('/bookings', bookingsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
