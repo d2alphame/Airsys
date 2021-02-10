fd94c0035c80d36b78623b7fc2cd3a95e9aa8692
Deji Adegbite <d2alphame@yahoo.com>
2021-02-10 15:49:45 +0100

    Add the bookings.js file

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,13 +1,20 @@
+// Contains routes for handling bookings.
   
 var express = require('express');
 var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 
-// open a database connection
+// Open a database connection
 let db = new sqlite3.Database('../flights.db');
 
-// 
+
+// Make request to this route make a booking 
 router.post('/', function(req, res, next) {
 
+})
+
+// Make a request to this route to cancel a booking
+router.post('/', function(req, res, next) {
+	
 })
 module.exports = router;
