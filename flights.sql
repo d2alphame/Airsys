c367d2d2cefa0074302f058ba7aec63cde426585
Deji Adegbite <d2alphame@yahoo.com>
2021-02-12 21:20:23 +0100

    Add foreign key to bookings table

--- flights.sql
+++ flights.sql
@@ -12,6 +12,7 @@ create table if not exists flights (
 
 create table if not exists bookings (
 	booking_id integer primary key,
+	flight_id integer not null,
 	booking_number integer not null,
 	flight_number integer not null,
 	seat_number char(4) not null,
@@ -19,5 +20,6 @@ create table if not exists bookings (
 	last_name varchar(32) not null,
 	middle_name varchar(32) not null,
 	email varchar(128) not null,
-	phone char(32)
+	phone char(32),
+	FOREIGN KEY(flight_id) REFERENCES flights(flight_id)
 );
