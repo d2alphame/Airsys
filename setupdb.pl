313ae6eb3477430045de6a209620f6282033c6c8
Deji Adegbite <d2alphame@yahoo.com>
2021-02-17 14:24:27 +0100

    List of airports

--- setupdb.pl
+++ setupdb.pl
@@ -1,16 +1,38 @@
 #!/usr/bin/perl
 
-use 5.26;
+use v5.26;
 use DBI;
 
 # Fill the flights table with some random flights
 my @cities = (
-	"Lagos",
-	"Ibadan",
-	"Illorin",
-	"PortHarcourt",
-	"Asaba",
-	"Jos",
 	"Abuja",
+	"Enugu",
+	"Ilorin",
+	"Kaduna",
+	"Kano",
+	"Lagos",
+	"Port Harcourt",
 	"Sokoto",
-)
+	"Asaba",
+	"Bauchi",
+	"Benin",
+	"Calabar",
+	"Ibadan",
+	"Jos",
+	"Maiduguri",
+	"Owerri",
+	"Uyo",
+	"Yola",
+	"Akure",
+	"Bauch",
+	"Gombe",
+	"Birnin Kebbi",
+	"Dutse",
+	"Jalingo",
+	"Katsina",
+	"Makurdi",
+	"Minna",
+	"Warri",
+	"Yenagoa",
+	"Zaria"
+);
