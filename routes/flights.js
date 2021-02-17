ec0dbcecb228f7e3b78ea2d2bb4006956addac7d
Deji Adegbite <d2alphame@yahoo.com>
2021-02-17 11:50:14 +0100

    Cities (WIP)

--- routes/flights.js
+++ routes/flights.js
@@ -9,5 +9,6 @@ let db = new sqlite3.Database('../flights.db');
 // Make a call to this route to create a flight schedule
 router.post('/', function(req, res, next) {
 
+	
 })
 module.exports = router;
--- setupdb.pl
+++ setupdb.pl
@@ -2,3 +2,15 @@
 
 use 5.26;
 use DBI;
+
+# Fill the flights table with some random flights
+my @cities = (
+	"Lagos",
+	"Ibadan",
+	"Illorin",
+	"PortHarcourt",
+	"Asaba",
+	"Jos",
+	"Abuja",
+	"Sokoto",
+)
