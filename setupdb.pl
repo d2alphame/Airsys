c93db9f896a0561d2119d48e5b8279507878127e
Deji Adegbite <d2alphame@yahoo.com>
2021-02-17 14:50:48 +0100

    Connect to the flights.db database

--- setupdb.pl
+++ setupdb.pl
@@ -3,6 +3,10 @@
 use v5.26;
 use DBI;
 
+# Number of random flights to fill the database with initially. For demo
+# purposes only.
+my $flightCount = 1000;
+
 # Fill the flights table with some random flights
 my @cities = (
 	"Abuja",
@@ -36,3 +40,12 @@ my @cities = (
 	"Yenagoa",
 	"Zaria"
 );
+
+
+# Connect to the flights.db database
+my $db = DBI -> connect("dbi:SQLite:dbname=flights.db", "", "");
+
+# Insert random flights into the flights table. For demo purposes only
+for($flightCount) {
+	
+}
