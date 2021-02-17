32655f40f512ec9fd05b861cdf09f0d94a9cf6b4
Deji Adegbite <d2alphame@yahoo.com>
2021-02-17 22:50:41 +0100

    Generate random date and month

--- setupdb.pl
+++ setupdb.pl
@@ -53,7 +53,27 @@ for($flightCount) {
 # Returns a random date.
 sub getRandomDate {
 
-	my $month = rand(12);			# Generate a random month
-	
+	my $month = int(rand(12)) + 1;			# Generate a random month
+	my $day;								# For a random date
+	if(		$month == 1		||
+			$month == 3		||
+			$month == 5		||
+			$month == 7		||
+			$month == 8		||
+			$month == 10	||
+			$month == 12	)
+	{
+		$day = int(rand(31)) + 1;
+	}
+	elsif(	$month == 4		||
+			$month == 6		||
+			$month == 9		||
+			$month == 11	)
+	{
+		$day = int(rand(30)) + 1;
+	}
+	elsif(	$month == 2	) {
+		$day = int(rand(28)) + 1;
+	}
 
 }
