fda13485ecd3c0d2c767860e65b87c3a0d9f2f5c
Deji Adegbite <d2alphame@yahoo.com>
2021-02-18 23:19:50 +0100

    Use datetime for takeoff time in flights.sql

--- flights.sql
+++ flights.sql
@@ -1,8 +1,7 @@
 create table if not exists flights (
 	flight_id integer primary key,
 	flight_number integer not null,
-	takeoff_date date not null,
-	takeoff_time time not null,
+	takeoff datetime not null,
 	takeoff_city varchar(128) not null,
 	takeoff_airport varchar(128) not null,
 	takeoff_terminal varchar(16) not null,
