daa60a8ed0b0df84131fd3b9302a3b67aece4bc2
Deji Adegbite <d2alphame@yahoo.com>
2021-03-18 20:13:18 +0100

    Very minor edit

--- routes/flights.js
+++ routes/flights.js
@@ -6,6 +6,7 @@ const sqlite3 = require('sqlite3').verbose();
 // open a database connection
 let db = new sqlite3.Database('../flights.db');
 
+
 // Make a call to this route to create a flight schedule
 router.post('/', function(req, res, next) {
 
