4c0388c4f97684aefffc70cb86d5d92ed3457e09
Deji Adegbite <d2alphame@yahoo.com>
2021-03-20 18:53:14 +0100

    Add textbox for takeoff city

--- routes/flights.js
+++ routes/flights.js
@@ -4,8 +4,17 @@ var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 
 // open a database connection
-let db = new sqlite3.Database('../flights.db');
+// let db = new sqlite3.Database('../flights.db');
 
+// Open a database connection
+var db = new sqlite3.Database(path.join(__dirname, '..', 'flights.db'), (err) => {
+	if(err) {
+		console.log(err)
+	}
+	else {
+		console.log("Success")
+	}
+});
 
 // Make a call to this route to create a flight schedule
 router.post('/', function(req, res, next) {
