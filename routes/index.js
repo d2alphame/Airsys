ddd34b9a830b463ac2495a76d900151e712aa811
Deji Adegbite <d2alphame@yahoo.com>
2021-03-25 16:27:11 +0100

    Require cities in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var cities = require('../cities');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', cities });
 });
 
 module.exports = router;
