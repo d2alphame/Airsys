ad0be5999782e0bf692c0275ad7e8ea5dcb32c2e
Deji Adegbite <d2alphame@yahoo.com>
2021-03-28 11:32:33 +0100

    WIP

--- routes/bookings.js
+++ routes/bookings.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
+var crypto = require('crypto')			// This will be necessary when generating booking number
 
 // Open a database connection
 let db = new sqlite3.Database('../flights.db');
@@ -14,7 +15,8 @@ router.post('/', function(req, res, next) {
 	
 	// Select the appropriate flight from the flight table
 	let sql = 'select * from flights where flight_number = ?'
-	db.get(sql, [req.body.flight], (err, row) => {
+	const { firstName, lastName, email, phone, flight } = req.body
+	db.get(sql, [flight], (err, row) => {
 		if(err) {
 			// TODO: Log error here
 			console.log(err)
@@ -30,7 +32,7 @@ router.post('/', function(req, res, next) {
 				let seatsLeft = row.seats_left--
 				
 				sql = 'update flights next_row = ?, next_col = ?, seats_left = ? where flight_number = ?'
-				db.run(sql, [nextRow, nextCol, seatsLeft, req.body.flight], (upError) => {
+				db.run(sql, [nextRow, nextCol, seatsLeft, flight], (upErr) => {
 					if(upErr) {
 						// TODO: Properly log error later
 						console.log(upErr)
@@ -38,7 +40,18 @@ router.post('/', function(req, res, next) {
 					else {
 						// Once the seat has been reserved, do the actual booking
 						sql = 'insert into bookings (flight_id, booking_number, flight_number, seat_number, first_name, last_name, middle_name, email, phone) values (?, ?, ?, ?, ?, ?, ?, ?, ?)'
-						db.run(sql, [req.body.flight, ])
+						let bookingNumber = crypto.randomBytes(4).toString('hex')
+						let seatNumber = "" + seatRow + seatCol
+						db.run(sql, [flight, bookingNumber, flight, seatNumber, firstName, lastName, "", req.body.email, req.body.phone], (insErr) => {
+							if(insErr) {
+								// TODO: Proper logging
+								console.log(insErr)
+								res.json({ error: "Your request could not be completed"})
+							}
+							else {
+								res
+							}
+						})
 					}
 				})
 			}
