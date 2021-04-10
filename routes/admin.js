60cb4e96683fdb156465ccb5488b7365f2a65183
Deji Adegbite <d2alphame@yahoo.com>
2021-04-10 11:28:50 +0100

    Working on admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+var path = require('path'); 
 
+// Open a database connection
+var db = new sqlite3.Database(path.join(__dirname, '..', 'flights.db'), (err) => {
+	if(err) {
+		// TODO: Use proper logging
+		console.log(err)
+	}
+	else {
+		// TODO: Use proper logging later
+		console.log("Success")
+	}
+});
 
 router.get('/', function(req, res, next) {
   res.render('admin', { title: 'Express' });
