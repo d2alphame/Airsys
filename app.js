feba568b9d97264f2ff5d586bedefa1fe0a3a85b
Deji Adegbite <d2alphame@yahoo.com>
2021-04-10 10:02:05 +0100

    Admin WIP

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var flightsRouter = require('./routes/flights');
 var bookingsRouter = require('./routes/bookings');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/flights', flightsRouter);
 app.use('/bookings', bookingsRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('admin', { title: 'Express' });
 });
 
 module.exports = router;
