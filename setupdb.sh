ea26f4652adcd2a5c8b6c7f1d10e404ae8bc452b
Deji Adegbite <d2alphame@yahoo.com>
2021-05-31 15:45:46 +0100

    Remove 'sudo' from install instructions

--- setupdb.sh
+++ setupdb.sh
@@ -2,10 +2,10 @@
 # Script to automatically setup the flights database
 
 # Install sqlite3
-sudo apt install sqlite3
+apt install sqlite3
 
 # Install DBI Perl module
-sudo apt install libdbi-perl
+apt install libdbi-perl
 
 # Select the flightsdb database (or create it if it doesn't exist) and run the sql queries in flights.sql
 sqlite3 flights.db < flights.sql
