0a8d84b8a5376eabe6034a2511f8ab86edced13a
Deji Adegbite <d2alphame@yahoo.com>
2021-05-31 17:01:41 +0100

    Remove installation of sqlite3 and dbi perl module

--- setupdb.sh
+++ setupdb.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 # Script to automatically setup the flights database
 
-# Install sqlite3
-apt install sqlite3
-
-# Install DBI Perl module
-apt install libdbi-perl
-
 # Select the flightsdb database (or create it if it doesn't exist) and run the sql queries in flights.sql
 sqlite3 flights.db < flights.sql
 ./setupdb.pl
