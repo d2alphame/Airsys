118a1d3ff300ce4555f1d53661f71a6dec228f9c
Deji Adegbite <d2alphame@yahoo.com>
2021-05-31 15:40:53 +0100

    Add installation of sqlite3 and dbi to build

--- setupdb.sh
+++ setupdb.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 # Script to automatically setup the flights database
 
+# Install sqlite3
+sudo apt install sqlite3
+
+# Install DBI Perl module
+sudo apt install libdbi-perl
+
 # Select the flightsdb database (or create it if it doesn't exist) and run the sql queries in flights.sql
 sqlite3 flights.db < flights.sql
 ./setupdb.pl
